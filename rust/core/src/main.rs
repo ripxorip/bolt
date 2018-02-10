ab759b7ba7b7c62f5ab0c1cd9f51a71c836aa4f6
Philip Karlsson <philipkarlsson@me.com>
2018-02-10 10:54:13 +0100

    Compiles, using scoping

--- rust/core/src/main.rs
+++ rust/core/src/main.rs
@@ -7,16 +7,19 @@ fn main() {
     /* There will always be a cwd */
     let cwd = env::current_dir().unwrap();
     let mut bolt = Bolt::new(cwd);
-    let r1 = &mut bolt;
     println!("--- Exp1 cwd ---");
-    println!("{}", r1.get_cwd(0));
+    {
+        println!("{}", bolt.get_cwd(0));
+    }
     println!("--- Listing exp1 ---");
-    let files = r1.get_listing(0);
-    for f in files {
-        println!("{}", f);
+    {
+        let files = bolt.get_listing(0);
+        for f in files {
+            println!("{}", f);
+        }
     }
     println!("--- Exp2 cwd ---");
-    r1.cd(1, -1);
+    bolt.cd(1, -1);
     println!("{}", bolt.get_cwd(1));
     println!("--- Listing exp2 ---");
     let files = bolt.get_listing(1);
