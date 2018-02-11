a04088ddece19e28865624ddc408495459e01f3b
Philip Karlsson <philipkarlsson@me.com>
2018-02-11 08:43:27 +0100

    Now rust->py getcwd works

--- rust/bolt.py
+++ rust/bolt.py
@@ -1,5 +1,5 @@
 import sys, ctypes
-from ctypes import c_char_p, c_uint32, Structure, POINTER
+from ctypes import c_void_p, c_char_p, c_uint32, c_int32, Structure, POINTER
 
 
 class BoltS(Structure):
@@ -10,6 +10,8 @@ class BoltS(Structure):
 lib = ctypes.cdll.LoadLibrary('interface/target/release/libbolt.dylib')
 lib.bolt_new.restype = POINTER(BoltS)
 lib.bolt_free.argtypes = (POINTER(BoltS), )
+lib.bolt_get_cwd.argtypes = (POINTER(BoltS), c_int32)
+lib.bolt_get_cwd.restype = (c_void_p)
 
 
 class Bolt:
@@ -22,6 +24,11 @@ class Bolt:
     def __exit__(self, exc_type, exc_value, traceback):
         lib.bolt_free(self.obj)
 
+    def get_cwd(self, id):
+        ptr = lib.bolt_get_cwd(self.obj, id)
+        return ctypes.cast(ptr, ctypes.c_char_p).value.decode('utf-8')
+
 
 if __name__ == "__main__":
-    test = Bolt()
+    bolt = Bolt()
+    print("Bolt CWD: " + bolt.get_cwd(0))
--- rust/interface/src/lib.rs
+++ rust/interface/src/lib.rs
@@ -1,5 +1,9 @@
 extern crate core;
+extern crate libc;
+
 use core::bolt::Bolt;
+use libc::{c_char, int32_t};
+use std::ffi::CString;
 
 #[no_mangle]
 pub extern fn bolt_new () -> *mut Bolt {
@@ -11,3 +15,13 @@ pub extern fn bolt_free (ptr: *mut Bolt) {
     if ptr.is_null() { return }
     unsafe { Box::from_raw(ptr); }
 } 
+
+#[no_mangle]
+pub extern fn bolt_get_cwd(ptr: *const Bolt, id: int32_t) -> *mut c_char {
+    let bolt = unsafe {
+        assert!(!ptr.is_null());
+        &*ptr
+    };
+    let c_str_ret = CString::new(bolt.get_cwd(id)).unwrap();
+    c_str_ret.into_raw()
+}
