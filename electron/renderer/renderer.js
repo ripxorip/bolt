6a1a630c272707aaead249c7fd573078679c7912
Philip Karlsson <philipkarlsson@me.com>
2018-02-12 17:54:31 +0100

    Initial commit of the electron GUI, although without any connection to the backend.

---
+++ electron/renderer/renderer.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-console */
+const fs = require('fs');
+const {shell} = require('electron');
+function readFolder(pth, backBtn) {
+    console.log('Ok');
+    if (backBtn) {
+        let path = `${backBtn}../`;
+        console.log(path);
+        document.getElementById('backBtn').innerHTML = `<button class="btn btn-small btn-default" onclick="readFolder(this.id);" id="${path}">Back</button><br>`;
+    }
+    fs.readdir(pth, (err, files) => {
+        'use strict';
+        if (err) throw err;
+        if (files[0] !== undefined) {
+            document.getElementById('listed-files').innerHTML = '<ul class="list-unstyled" id="files"></ul>';
+            for (let file of files) {
+                let theID = `${pth}${file}/`;
+                fs.stat(pth + file, (err, stats) => {
+                    if (err) throw err;
+
+                    if (stats.isDirectory()) {
+                        document.getElementById('files').innerHTML += `<li id=${theID} ondblclick="readFolder(this.id,this.id);"><span class="displayed-files"  ><i class="fa fa-folder-open" aria-hidden="true"></i> ${file}</span></li><hr>`;
+                    }
+                    else {
+                        document.getElementById('files').innerHTML += `<li id=${theID} ondblclick="openFile(this.id);"><span class="displayed-files"   ><i class="fa fa-file" aria-hidden="true" ></i> ${file}</span></li><hr>`;
+                    }
+
+                });
+            }
+        }
+        else {
+            document.getElementById('listed-files').innerHTML = 'Empty Folder';
+        }
+    });
+}
+
+function openFile(path) {
+    'use strict';
+    console.log(path);
+    shell.openItem(path);
+}
+
+//$(document).on("keydown", function (e) {
+//    if ((e.ctrlKey) && e.which === 70) {
+      
+        //70 is keycode for F http://www.alt-codes.net/
+//        console.log("user pressed ctrl+F buttons") ;
+
+//    }
+//});
