ee9487b73dca2cd44d6947a98a807dd5d7f09653
Philip Karlsson <philipkarlsson@me.com>
2018-02-12 19:21:49 +0100

    WIP with proper linting etc.

---
+++ electron/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "standard",
+    "rules": {
+      "semi": "off",
+      "space-before-function-paren" : "off",
+      "camelcase": "off",
+      "eqeqeq": "off"
+    }
+};
--- electron/main.js
+++ electron/main.js
@@ -1,1 +1,6 @@
- 
+const {app, BrowserWindow} = require('electron');
+
+app.on('ready', () => {
+  const window = new BrowserWindow({width: 800, height: 600});
+  window.loadURL(`file://${__dirname}/html/index.html`);
+});
